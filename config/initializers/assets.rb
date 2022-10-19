75590b80905e6b212467ac228aa34f698b5778ba
Johannes Schäffer <schaeffer.johannes@gmail.com>
2022-10-19 23:58:22 +0200

    fix asset init bug

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,7 +10,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.paths << Rails.root.join('vendor')
 
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')  
-Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+Rails.application.config.assets.precompile << ["*.svg", "*.eot", "*.woff", "*.ttf"]
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
