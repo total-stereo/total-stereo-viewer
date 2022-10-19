ee6aa16b70ae559f2c7dd3b2575001d049c04117
Johannes Schäffer <schaeffer.johannes@gmail.com>
2022-10-19 16:46:20 +0200

    new design

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,19 +4,29 @@ class HomeController < ApplicationController
   end
 
   def index
-    @image_names = image_names
+    
+    @mode = params[:mode] || "stereo"
+    
+    if @mode == "stereo"
+      @image_names = stereo_image_names
+    elsif @mode == "dia"
+      @image_names = dia_image_names
+    elsif @mode == "anaglyph"
+      @image_names = anaglyph_image_names
+    end
+
     #@images = Dir.entries("#{Rails.public_path}/stereo_imgs/images")
     #@images = Find.find("#{Rails.public_path}/stereo_imgs/images/")
   end
 
   def show
     index = params[:i].to_i
-    @file_name = image_names[index]
+    @file_name = stereo_image_names[index]
     @imgpath = "stereo_imgs/images/" + @file_name
     @mappath = "stereo_imgs/depth_maps/" + @file_name
     
     @prev = index > 0 ? index - 1 : nil
-    @next = index < image_names.size - 1 ? index+1 : nil 
+    @next = index < stereo_image_names.size - 1 ? index+1 : nil 
     
     #render "show", layout: false
   end
@@ -27,10 +37,23 @@ class HomeController < ApplicationController
   def about
   end
   
+  def about_collection
+  end
+  
   private
-  def image_names
-    Rails.cache.fetch("image_names_file_list", expires_in: 30.minutes) do
+  def stereo_image_names
+    Rails.cache.fetch("stereo_file_list", expires_in: 30.minutes) do
       return Dir.glob("#{Rails.public_path}/../app/assets/images/stereo_imgs/images/*").each{|str| str.gsub!("#{Rails.public_path}/../app/assets/images/stereo_imgs/images/","")}.sort
+    end
+  end
+  def dia_image_names
+    Rails.cache.fetch("dia_file_list", expires_in: 30.minutes) do
+      return Dir.glob("#{Rails.public_path}/../app/assets/images/dia_imgs/images/*").each{|str| str.gsub!("#{Rails.public_path}/../app/assets/images/dia_imgs/images/","")}.sort
+    end
+  end
+  def anaglyph_image_names
+    Rails.cache.fetch("anaglyph_file_list", expires_in: 30.minutes) do
+      return Dir.glob("#{Rails.public_path}/../app/assets/images/anaglyph_imgs/images/*").each{|str| str.gsub!("#{Rails.public_path}/../app/assets/images/anaglyph_imgs/images/","")}.sort
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'index/:mode', to: "home#index"
   get 'index', to: "home#index"
   get 'show/:i', to: "home#show", as: "show"
   get 'upload', to: "home#upload"
   get 'about', to: "home#about"
+  get 'about-collection', to: "home#about_collection"
   
   root 'home#start'
 end
